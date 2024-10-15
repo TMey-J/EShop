3f91e464d4b1680641b115cf0255ee994371f820
TMey-J <tahameymaneh@gmail.com>
2024-10-15 16:13:45 +0330

    add configurations for identity entities

--- src/EShop.Infrastucture/Databases/SQLDbContext.cs
+++ src/EShop.Infrastucture/Databases/SQLDbContext.cs
@@ -7,6 +7,11 @@ namespace EShop.Infrastucture.Databases
     public class SQLDbContext(DbContextOptions options) :
         IdentityDbContext<User,Role,long,UserClaim,UserRole,UserLogin,RoleClaim,UserToken>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(SQLDbContext).Assembly);
 
+        }
     }
 }
