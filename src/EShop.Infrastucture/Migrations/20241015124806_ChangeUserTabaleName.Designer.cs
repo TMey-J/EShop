f16fe98f0f436704a732007608ca3c8dd0082473
TMey-J <tahameymaneh@gmail.com>
2024-10-22 14:23:12 +0330

    some change

--- src/EShop.Infrastucture/Migrations/20241015124806_ChangeUserTabaleName.Designer.cs
+++ src/EShop.Infrastucture/Migrations/20241015124806_ChangeUserTabaleName.Designer.cs
@@ -4,6 +4,7 @@ using EShop.Infrastucture.Databases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EShop.Infrastucture.Migrations
 {
     [DbContext(typeof(SQLDbContext))]
-    partial class SQLDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241015124806_ChangeUserTabaleName")]
+    partial class ChangeUserTabaleName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/EShop.Infrastucture/Migrations/20241015124806_ChangeUserTabaleName.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserTabaleName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserClaims_AspNetUsers_UserId",
+                table: "UserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLogins_AspNetUsers_UserId",
+                table: "UserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_AspNetUsers_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_AspNetUsers_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "User");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserClaims_User_UserId",
+                table: "UserClaims",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLogins_User_UserId",
+                table: "UserLogins",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_User_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_User_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserClaims_User_UserId",
+                table: "UserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLogins_User_UserId",
+                table: "UserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_User_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_User_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "AspNetUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserClaims_AspNetUsers_UserId",
+                table: "UserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLogins_AspNetUsers_UserId",
+                table: "UserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_AspNetUsers_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_AspNetUsers_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
