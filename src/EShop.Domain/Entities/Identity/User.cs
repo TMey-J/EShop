2083dad6b4df6e44ab33b3f3c1abdb51df10104f
TMey-J <tahameymaneh@gmail.com>
2024-10-23 16:40:02 +0330

    change phone number max length

--- src/EShop.Domain/Entities/Identity/User.cs
+++ src/EShop.Domain/Entities/Identity/User.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Domain.Entities.Identity
 {
     public class User: IdentityUser<long>
     {
+        [MaxLength(11)]
+        public override string? PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
         #region Relations
 
         public virtual ICollection<UserClaim>? UserClaims { get; set; }
