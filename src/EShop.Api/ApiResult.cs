2d74e61439533d5ea3772d68fb57531831fb5619
TMey-J <tahameymaneh@gmail.com>
2024-10-24 16:30:38 +0330

    add api costum response

---
+++ src/EShop.Api/ApiResult.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Application.Constants.Common;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace EShop.Api
+{
+    public class ApiResult
+        (bool isSuccess, HttpStatusCode statusCode, string? messege = Messages.Successful, object? data = null)
+    {
+        public bool IsSuccess { get; set; } = isSuccess;
+        public HttpStatusCode StatusCode { get; set; } = statusCode;
+        public string? Messege { get; set; } = messege;
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public object? Data { get; set; } = data;
+    }
+}
