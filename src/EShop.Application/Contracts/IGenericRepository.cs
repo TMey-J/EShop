a06fe722c5641c2effb5598fe4f387711158b501
TMey-J <tahameymaneh@gmail.com>
2024-11-01 19:45:30 +0330

    create generic repository

---
+++ src/EShop.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using EShop.Domain.Entities;
+
+namespace EShop.Application.Contracts;
+
+public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+    Task CreateAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    void SoftDeleteAsync(TEntity entity);
+    Task<TEntity?> FindByIdAsync(long id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task SaveChangesAsync();
+
+
+}
---
+++ src/EShop.Infrastucture/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using EShop.Application.Contracts;
+using EShop.Domain.Entities;
+using EShop.Infrastucture.Databases;
+
+namespace EShop.Infrastucture.Repositories;
+
+internal class GenericRepository<TEntity>(SQLDbContext context) : IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+    private readonly DbSet<TEntity> _dbSet=context.Set<TEntity>();
+    private readonly SQLDbContext _context =context;
+    public async Task CreateAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+         _dbSet.Remove(entity);
+    }
+
+    public async Task<TEntity?> FindByIdAsync(long id)
+    {
+        return await _dbSet.SingleOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+       await _context.SaveChangesAsync();
+    }
+
+    public void SoftDeleteAsync(TEntity entity)
+    {
+        entity.IsDelete = true;
+        Update(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+         _dbSet.Update(entity);
+    }
+}
