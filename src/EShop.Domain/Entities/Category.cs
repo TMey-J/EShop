7da0ba69803303b072c60866e24e139ee2807f78
TMey-J <tahameymaneh@gmail.com>
2024-11-01 19:16:20 +0330

    update category entity

--- src/EShop.Domain/Entities/Category.cs
+++ src/EShop.Domain/Entities/Category.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Domain.Entities
 {
     public class Category:BaseEntity
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }=string.Empty;
+        [Required]
         public HierarchyId Parent { get; set; }
+        [MaxLength(40)]
         public string? Picture { get; set; }
     }
 }
