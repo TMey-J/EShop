7ab8522f84723d81cbd2ca4557ea1fe1696b98a6
TMey-J <tahameymaneh@gmail.com>
2024-11-10 15:52:21 +0330

    add pagination

---
+++ src/EShop.Application/Common/GenericPagitionExtention.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Application.Common;
+
+public static class GenericPagitionExtention
+{
+    public static (IQueryable<T>,int) Page<T>(this IQueryable<T> query, int currentPage, int takeRecord)
+    {
+        var pageCount = (int)Math.Ceiling((double)query.Count() / takeRecord);
+        if (currentPage>pageCount)
+            currentPage = pageCount;
+        return (query.Skip((currentPage - 1) * takeRecord).Take(takeRecord),pageCount);
+    }
+}
