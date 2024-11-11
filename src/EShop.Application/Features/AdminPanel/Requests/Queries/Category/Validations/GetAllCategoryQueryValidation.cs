f7942c68c47ce059e95d4c274831faabba22a32f
TMey-J <tahameymaneh@gmail.com>
2024-11-11 10:23:54 +0330

    create validation for GetAllCategoryQuery

---
+++ src/EShop.Application/Features/AdminPanel/Requests/Queries/Category/Validations/GetAllCategoryQueryValidation.cs
@@ -0,0 +1,27 @@
+﻿using EShop.Application.Features.AdminPanel.Requests.Commands.Category;
+using FluentValidation;
+
+namespace EShop.Application.Features.AdminPanel.Requests.Queries.Category.Validations
+{
+    public class GetAllCategoryQueryValidation : AbstractValidator<GetAllCategoryQueryRequest>
+    {
+        public GetAllCategoryQueryValidation()
+        {
+            RuleFor(x => x.Search.Title).
+                MaximumLength(100).WithMessage(Messages.Validations.MaxLength);
+            
+            RuleFor(x => x.Search.SortingBy).IsInEnum().
+            WithMessage(Messages.Validations.NotInEnum(0,1));
+            
+            RuleFor(x => x.Search.DeleteStatus).IsInEnum().
+                WithMessage(Messages.Validations.NotInEnum(0,1));
+            
+            RuleFor(x => x.Search.SortingAs).IsInEnum().
+                WithMessage(Messages.Validations.NotInEnum(0,1));
+
+            RuleFor(x => x.Search.Pagination.TakeRecord).
+                InclusiveBetween(1, 100).WithMessage(Messages.Validations.Between);
+
+        }
+    }
+}
