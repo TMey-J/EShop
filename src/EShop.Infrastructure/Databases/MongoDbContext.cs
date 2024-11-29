28bc1216de9f373324a6cb4d9b0675035cc6ddb0
TMey-J <tahameymaneh@gmail.com>
2024-11-29 11:34:47 +0330

    add and configure mongodb to project

---
+++ src/EShop.Infrastructure/Databases/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace EShop.Infrastructure.Databases;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IConfiguration config)
+    {
+        var client = new MongoClient(config.GetConnectionString("MongoDbConnection"));
+        _database = client.GetDatabase(config.GetSection("MongoDb:DatabaseName").Value);
+    }
+    public IMongoCollection<TEntity> GetCollection<TEntity>()
+    {
+        return _database.GetCollection<TEntity>(typeof(TEntity).Name.ToLower());
+    }
+}
